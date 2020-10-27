3e041f8ed198e936a9aa3194e95afc318b71bfae
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-27 00:58:01 -0400

    few chnages

--- test_calculator.py
+++ test_calculator.py
@@ -9,4 +9,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_substraction2(self):
-        assert 100 == calculator.subtract(101, 1)
+        assert 100 == calculator.subtract(200, 100)
