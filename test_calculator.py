67782bd7784e52be05e4e82258e0b3726cb70819
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-27 00:37:12 -0400

    test

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+import calculator
+
+
+class TestCalculator:
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_substraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+    def test_substraction2(self):
+        assert 100 == calculator.subtract(101, 1)
